ac12fbfd8e61e8292ba7fb104f013ff0844f2bf3
Arjinderpurba <arjinderpurba02@gmail.com>
2025-09-10 20:02:47 +0530

    writing auth middleware

--- src/app.js
+++ src/app.js
@@ -1,12 +1,24 @@
-const express = require('express');
+const express = require("express");
+const { adminAuth, userAuth } = require("./middlewares/auth");
 
 const app = express();
 
-app.get("/user/:userId/:name/:passsword",(req, res) => {
-    console.log(req.params);
-    res.send({firstName: "Arjinder", lastName: "Singh"});
+app.use("/admin", adminAuth);
+// app.use("/user", userAuth);
+
+app.post("/user/login", (req, res) => {
+    res.send("User logged in Successfully");
 })
 
+app.get("/user/data", userAuth, (req, res) => {
+    res.send("All data Sent");
+});
+
+app.get("/admin/getAllData",(req, res) => {
+    res.send("All data Sent");
+});
+
+
 app.listen(3000, () => {
-    console.log("Server is successfully listening on port 3000...");
+  console.log("Server is successfully listening on port 3000...");
 });
