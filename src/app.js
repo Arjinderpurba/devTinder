a13786b99005f747f078f07036c0124fd46456d8
Arjinderpurba <arjinderpurba02@gmail.com>
2025-09-10 13:05:40 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,21 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.get("/user",(req, res) => {
+app.get("/user/:userId/:name/:passsword",(req, res) => {
+    console.log(req.params);
     res.send({firstName: "Arjinder", lastName: "Singh"});
 })
-
-app.post("/user",(req, res) => {
-    res.send("Data successfully saved in the DB");
-})
-
-app.delete("/user",(req, res) => {
-    res.send("Data Deleted successfully from the DB");
-})
-
-app.use("/user",(req, res) => {
-    res.send("It's Available...")
-});
 
 app.listen(3000, () => {
     console.log("Server is successfully listening on port 3000...");
