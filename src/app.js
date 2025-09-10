9a87e68f2f279df3c2f77580dd0e748b0a12425f
Arjinderpurba <arjinderpurba02@gmail.com>
2025-09-10 08:17:40 +0530

    initialized a backend server

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+app.use("/server",(req, res) => {
+    res.send("Server ton boldan...")
+});
+
+app.use("/test",(req, res) => {
+    res.send("mic testing...")
+});
+
+app.use("/available",(req, res) => {
+    res.send("It's Available...")
+});
+
+app.listen(3000, () => {
+    console.log("Server is successfully listening on port 3000...");
+});
+
