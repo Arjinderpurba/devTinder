54049e936e64919946f08ef3dbe80012ab7b9a27
Arjinderpurba <arjinderpurba02@gmail.com>
2025-09-16 13:38:12 +0530

    update user data API

--- src/app.js
+++ src/app.js
@@ -25,14 +25,14 @@ app.get("/user", async (req, res) => {
     const userEmail = req.body.emailId;
 
     try{
-        const users = await User.findOne({emailId: userEmail});
+        const users = await User.find({emailId: userEmail});
         if(users.length === 0){
             res.status(404).send("User not found");
         } else {
             res.send(users);
         }
     }
-    catch{
+    catch(err){
         res.status(400).send("Something went wrong");
     }
 });
@@ -42,10 +42,36 @@ app.get("/feed", async (req, res) => {
     try{
         const users = await User.find({});
         res.send(users);
-    } catch {
+    } catch(err){
         res.status(400).send("Something went wrong");
     }
 })
+
+// Delete User API
+app.delete("/user", async (req, res) => {
+    const userId = req.body.userId;
+
+    try{
+        const user  = await User.findByIdAndDelete(userId);
+        res.send("User deleted successfully");
+    } catch(err){
+        res.status(400).send("Something went wrong");
+    }
+});
+
+// Update data of the user
+app.patch("/user", async (req, res) => {
+    const userId = req.body.userId;
+    const data = req.body;
+
+    try{
+        await User.findByIdAndUpdate({ _id: userId },data);
+        res.send("User data updated successfully");
+    } catch(err){
+        res.status(400).send("Something went wrong");
+    }
+});
+
 
 connectDB()
   .then(() => {
