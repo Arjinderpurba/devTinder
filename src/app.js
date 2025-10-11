5f88e8de59f0696f3e03b60b0acbd369f076fe50
Arjinderpurba <arjinderpurba02@gmail.com>
2025-10-11 11:43:44 +0530

    validateed api's and used npm valiadate library for validations

--- src/app.js
+++ src/app.js
@@ -8,7 +8,6 @@ const app = express();
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-    
   //Creating a new instance of User model
   const user = new User(req.body);
 
@@ -22,58 +21,81 @@ app.post("/signup", async (req, res) => {
 
 //get user by email
 app.get("/user", async (req, res) => {
-    const userEmail = req.body.emailId;
+  const userEmail = req.body.emailId;
 
-    try{
-        const users = await User.find({emailId: userEmail});
-        if(users.length === 0){
-            res.status(404).send("User not found");
-        } else {
-            res.send(users);
-        }
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
     }
-    catch(err){
-        res.status(400).send("Something went wrong");
-    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
 });
 
 // Feed API - GET /Feed - get all users from database
 app.get("/feed", async (req, res) => {
-    try{
-        const users = await User.find({});
-        res.send(users);
-    } catch(err){
-        res.status(400).send("Something went wrong");
-    }
-})
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
 
 // Delete User API
 app.delete("/user", async (req, res) => {
-    const userId = req.body.userId;
+  const userId = req.body.userId;
 
-    try{
-        const user  = await User.findByIdAndDelete(userId);
-        res.send("User deleted successfully");
-    } catch(err){
-        res.status(400).send("Something went wrong");
-    }
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
 });
 
 // Update data of the user
-app.patch("/user", async (req, res) => {
-    const userId = req.body.userId;
-    const data = req.body;
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
 
-    try{
-        await User.findByIdAndUpdate({ _id: userId },data);
-        res.send("User data updated successfully");
-    } catch(err){
-        res.status(400).send("Something went wrong");
+  try {
+    const ALLOWED_UPDATES = [
+        "photoUrl",
+        "about",
+        "gender",
+        "age",
+        "skills",
+    ];
+
+    const isUpdatesAllowed = Object.keys(data).every((k) => 
+        ALLOWED_UPDATES.includes(k)
+    );
+
+    if(!isUpdatesAllowed) {
+        throw new Error("Update not allowed");    
     }
+
+    if(data?.skills.length > 10){
+        throw new Error("you can only add 10  skills")
+    }
+
+
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+
+    res.send("User data updated successfully");
+  } catch (err) {
+    res.status(400).send("UPDATE FAILED:" + err.message);
+  }
 });
 
-
-connectDB()
+connectDB() 
   .then(() => {
     console.log("Database connection is established");
     app.listen(3000, () => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,72 @@
 const mongoose = require("mongoose");
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
+    required: true,
+    trim: true,
   },
   lastName: {
     type: String,
+    trim: true,
   },
   emailId: {
     type: String,
+    required: true,
+    unique: true, 
+    lowercase: true,
+    trim: true,
+    minLength: 3,
+    maxLength: 50,
+    validate(value){
+      if(!validator.isEmail(value)){
+        throw new Error("invalid email address: " + value);
+      }
+    }
   },
   password: {
     type: String,
+    required: true,
+    // validate(value){
+    //   if(!validator.isStrongPassword(value)){
+    //     throw new Error("Enter a srong password: " + value);
+    //   }
+    // }
   },
   age: {
     type: Number,
+    min: 18,
+    trim: true,
   },
   gender: {
     type: String,
+    validate(value) {
+      if (!["male","female","others"].includes(value)){
+        throw new Error("Gender data is not valid");
+      }
+    },
   },
+  photoUrl: {
+    type: String,
+    default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    validate(value){
+      if(!validator.isURL(value)){
+        throw new Error("invalid photo Url " + value);
+      }
+    }
+  }, 
+  about: {
+    type: String,
+    default: "This is a default about of the user!",
+    trim: true,
+  },
+  skills: {
+    type: [String],
+    trim: true,
+  }
+}, {
+  timestamps: true,
 });
 
 const User = mongoose.model("User", userSchema);
